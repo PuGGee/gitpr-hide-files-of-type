6faf8a509b0de076856eb22867e60839fb977b27
Lewis Wrigley <lewis@arenaflowers.com>
2018-02-02 14:24:00 +0000

    Allow hiding spec and test files
      Makes a special case for spec and test files, since they
      often get in the way during review, or may need to be
      reviewed seperately from the rest of the code

--- github.com.js
+++ github.com.js
@@ -4,11 +4,13 @@
     var container = document.querySelector('.pr-toolbar');
 
     var hideShowFiles = function(file_extension, hide) {
+      var escaped_file_extension = file_extension.replace(/[\-\[\]\/\{\}\(\)\+\?\.\\\^\$\|]/g, "\\$&");
+
       var file_links = document.querySelectorAll('.file-info a')
       for (var i = 0; i < file_links.length; i++) {
         var link = file_links[i];
-        var extension_match = link.text.match(/\.(\w+)$/);
-        if (extension_match && extension_match[1] == file_extension) {
+        var extension_match = link.text.match(new RegExp(escaped_file_extension.replace(/\*/gi, '.*') + '$'));
+        if (extension_match) {
           var el = link;
           while ((el = el.parentElement) && !el.classList.contains('js-file'));
           el.style.display = (hide ? 'none' : null);
@@ -25,21 +27,24 @@
       }
 
       var label = document.createElement('label')
-      label.appendChild(document.createTextNode('Hide .' + file_extension + ' files'));
+      label.appendChild(document.createTextNode('Hide ' + file_extension + ' files'));
 
-      container.appendChild(checkbox);
       container.appendChild(label);
+      container.appendChild(checkbox);
     }
 
     var file_links = document.querySelectorAll('.file-info a')
     var extensions = []
     for (var i = 0; i < file_links.length; i++) {
       var link = file_links[i];
-      var extension_match = link.text.match(/\.(\w+)$/);
+      var extension_match = link.text.match(/(\.\w+)$/);
       if (extension_match && extensions.indexOf(extension_match[1]) == -1) {
         extensions.push(extension_match[1])
         createCheckbox(extension_match[1]);
       }
     }
+
+    createCheckbox('test.*');
+    createCheckbox('spec.*');
   }, 3000);
 })(document);
